d087aaa96d98350f9b63d71655b442473229a7f0
rnipa <74035707+rnipa@users.noreply.github.com>
2026-08-05 22:50:43 +0600

    service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,62 @@
-const CACHE_NAME = 'pos-finance-erp-v1';
+const CACHE_NAME = "customer-ledger-v3.3";
 
 const urlsToCache = [
-'./',
-'./index.html',
-'./manifest.json',
-'./icon-192.png',
-'./icon-512.png'
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
-self.addEventListener('install', event => {
-event.waitUntil(
-caches.open(CACHE_NAME)
-.then(cache => cache.addAll(urlsToCache))
-);
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Caching App Files...");
+      return cache.addAll(urlsToCache);
+    })
+  );
+
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', event => {
-event.respondWith(
-caches.match(event.request)
-.then(response => response || fetch(event.request))
-);
+// Activate
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
+  );
+
+  self.clients.claim();
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request)
+          .then((response) => {
+            const copy = response.clone();
+
+            caches.open(CACHE_NAME).then((cache) => {
+              cache.put(event.request, copy);
+            });
+
+            return response;
+          })
+          .catch(() => cached)
+      );
+    })
+  );
 });
