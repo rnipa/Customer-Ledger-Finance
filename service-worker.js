0fd8ebc0f0dc7cbd23caa2eea7680dae4aff5678
rnipa <74035707+rnipa@users.noreply.github.com>
2026-08-15 00:35:38 +0600

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,8 +0,0 @@
-const CACHE_NAME='customer-ledger-v1';
-const FILES=['./','./index.html','./manifest.json'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME&&caches.delete(k))))));
-self.addEventListener('fetch',e=>{
- if(e.request.method!=='GET')return;
- e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
-});
